e9a4b408f071633ffc2374baa3783b9c99e51e05
IshaaS <isha.sasturkar@gmail.com>
2025-06-26 16:58:43 -0400

    deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,29 +5,29 @@ import cors from "cors";
 
 import path from "path";
 
-import {app, server} from "./lib/socket.js";
-
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
 import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials:true,
-}
-))
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-server.listen(PORT,()=>{
-    console.log("server is running on port:"+ PORT);
-    connectDB();
-}); 
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
